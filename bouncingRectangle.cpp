83e7566fe71bf7efd0e11ea32855af1f7a5c1432
Owen O'Connor <oconnor.57@gmail.com>
2020-04-15 19:34:09 -0400

    Draw FPS instead of printing it :)

--- bouncingRectangle.cpp
+++ bouncingRectangle.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <iostream>
 #include <math.h>
+#include <cstring>
 
 void drawRectangle(float x, float y, float width, float height, float r, float g, float b) {
     glColor3f(r, g, b);
@@ -11,6 +12,14 @@ void drawRectangle(float x, float y, float width, float height, float r, float g
     glVertex2f(x+width, y);
     glVertex2f(x+width, y+height);
     glVertex2f(x, y+height);
+    glEnd();
+}
+
+void drawString(float x, float y, char *str) {
+    glColor3f(1, 1, 1);
+    glRasterPos2f(x, y);
+    for(size_t i = 0; i < strlen(str); ++i)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *(str + i));
     glEnd();
 }
 
@@ -27,15 +36,16 @@ void setView() {
 void display() {
     // Calculate and print fps
     static double lastTime = 0;
-    static long numFrames = 0;
+    static int numFrames = 0;
+    static int currentFPS = 0;
 
     double timeNow = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now().time_since_epoch()).count();
     
     ++numFrames;
 
     if(timeNow - lastTime > 1) {
-        std::cout << numFrames << "fps" << std::endl;
         lastTime = timeNow;
+        currentFPS = numFrames;
         numFrames = 0;
     }
 
@@ -45,6 +55,11 @@ void display() {
     setView();
 
     drawRectangle(100, 100, 100, 100 + 50 * sin(timeNow * 10), 0, 1, 0.5f);
+    
+    char *fpsStr = new char[24];
+    sprintf(fpsStr, "Hello OpenGL!  %dfps", currentFPS);
+    drawString(5, 5, fpsStr);
+    delete[] fpsStr;
 
     glutSwapBuffers();
 }
